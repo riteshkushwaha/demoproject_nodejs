2599058a45dee1dc4135d54bc27b50d6546a63d4
riteshkushwaha <riteshkush@gmail.com>
2022-11-18 12:03:35 +0530

    updating project json file and dependents

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,14 @@
-console.log("hello node js")
+console.log("hello node js...")
+
+const express = require('express')
+const app = express()
+
+//settting up router
+app.get('/hello',(req,res)=>{
+    res.send('Task manager app')
+})
+
+// set port number
+const port = 3000
+//setting server listen port no
+app.listen(port,console.log(`server is listening on port ${port}`))
